28ebcaf3c18278d101e51db7217cf4b696189c20
Kristen Schau <47155823+krschau@users.noreply.github.com>
2022-07-18 14:50:00 -0700

    Add webview2 content to winui 2 app (winui 2)

--- NativeWebWinUI2App/NativeWebWinUI2App/MainPage.xaml.cs
+++ NativeWebWinUI2App/NativeWebWinUI2App/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -12,6 +17,85 @@ namespace NativeWebWinUI2App
         public MainPage()
         {
             this.InitializeComponent();
+            AddressBar.Text = "https://developer.microsoft.com/en-us/microsoft-edge/webview2/";
+            webView2.Source = new Uri(AddressBar.Text);
+
+            webView2.NavigationCompleted += WebView2_NavigationCompleted;
+
+            StatusUpdate("Ready");
+        }
+
+        private void StatusUpdate(string message)
+        {
+            StatusBar.Text = message;
+        }
+
+        private void WebView2_NavigationCompleted(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+        {
+            StatusUpdate("Navigation complete");
+        }
+
+        private bool TryCreateUri(String potentialUri, out Uri result)
+        {
+            StatusUpdate("TryCreateUri");
+
+            Uri uri;
+            if ((Uri.TryCreate(potentialUri, UriKind.Absolute, out uri) || Uri.TryCreate("http://" + potentialUri, UriKind.Absolute, out uri)) &&
+                (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+            {
+                result = uri;
+                return true;
+            }
+            else
+            {
+                StatusUpdate("Unable to configure URI");
+                result = null;
+                return false;
+            }
+        }
+
+        private void TryNavigate()
+        {
+            StatusUpdate("TryNavigate");
+
+            Uri destinationUri;
+            if (TryCreateUri(AddressBar.Text, out destinationUri))
+            {
+                webView2.Source = destinationUri;
+            }
+            else
+            {
+                StatusUpdate("URI couldn't be figured out use it as a bing search term");
+
+                String bingString = "https://www.bing.com/search?q=" + Uri.EscapeUriString(AddressBar.Text);
+                if (TryCreateUri(bingString, out destinationUri))
+                {
+                    AddressBar.Text = destinationUri.AbsoluteUri;
+                    webView2.Source = destinationUri;
+                }
+                else
+                {
+                    StatusUpdate("URI couldn't be configured as bing search term, giving up");
+                }
+            }
+        }
+
+        private void Go_OnClick(object sender, RoutedEventArgs e)
+        {
+            StatusUpdate("Go_OnClick: " + AddressBar.Text);
+
+            TryNavigate();
+        }
+
+        private void AddressBar_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                StatusUpdate("AddressBar_KeyDown [Enter]: " + AddressBar.Text);
+
+                e.Handled = true;
+                TryNavigate();
+            }
         }
     }
 }
