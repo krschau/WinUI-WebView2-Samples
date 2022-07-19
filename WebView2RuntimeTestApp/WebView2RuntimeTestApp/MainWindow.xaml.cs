c393a65a708155893211997bf90a5b83beff6bbe
Kristen Schau <47155823+krschau@users.noreply.github.com>
2022-07-19 10:24:05 -0700

    Call GetAvailableCoreWebView2BrowserVersionString

--- WebView2RuntimeTestApp/WebView2RuntimeTestApp/MainWindow.xaml.cs
+++ WebView2RuntimeTestApp/WebView2RuntimeTestApp/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,9 +15,11 @@ namespace WebView2RuntimeTestApp
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void getVersionButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            string browserVersionString = Microsoft.Web.WebView2.Core.CoreWebView2Environment.GetAvailableBrowserVersionString(string.Empty);
+
+            versionText.Text = browserVersionString;
         }
     }
 }
